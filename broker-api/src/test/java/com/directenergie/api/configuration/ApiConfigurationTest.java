e084498910e172588c8b8c01dfaa4d70ff1ab6da
Romain BIARD <romain.biard@orange.com>
2013-11-23 04:06:16 +0100

    Ignore test (to be moved in Core)

--- broker-api/src/test/java/com/directenergie/api/configuration/ApiConfigurationTest.java
+++ broker-api/src/test/java/com/directenergie/api/configuration/ApiConfigurationTest.java
@@ -1,17 +1,17 @@
 package com.directenergie.api.configuration;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.directenergie.api.configuration.ApiConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { ApiConfiguration.class })
 public class ApiConfigurationTest {
 
 	@Test
+	@Ignore
 	public void dummy() {
 
 	}
