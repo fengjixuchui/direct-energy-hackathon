32886f582e2829982d94c6500e0a2fd5585023d1
Antoine Cœur <coeur@gmx.fr>
2013-11-23 01:03:26 +0100

    initial iOS commit

---
+++ iOS/MoneyForPower/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoneyForPower
+//
+//  Created by Antoine Cœur on 23/11/2013.
+//  Copyright (c) 2013 Cœur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/MoneyForPower/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MoneyForPower
+//
+//  Created by Antoine Cœur on 23/11/2013.
+//  Copyright (c) 2013 Cœur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
