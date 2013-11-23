12d89e0752c4743d6cbe1be7d7122c48fe8ac0df
yoann792 <yoann792@gmail.com>
2013-11-23 21:59:27 +0100

    Appel de  la classe d’appel ws domotique depuis la class EffacementService

--- broker-core/src/main/java/com/directenergie/core/services/EffacementService.java
+++ broker-core/src/main/java/com/directenergie/core/services/EffacementService.java
@@ -1,6 +1,5 @@
 package com.directenergie.core.services;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,11 +7,8 @@ import javax.inject.Inject;
 import com.directenergie.core.model.Device;
 import com.directenergie.core.model.User;
 import com.directenergie.core.repository.UserRepository;
-import com.notnoop.exceptions.InvalidSSLConfig;
-import com.notnoop.exceptions.RuntimeIOException;
 
 public class EffacementService {
-
 	
 	@Inject
 	private UserRepository userRepository;
@@ -20,28 +16,43 @@ public class EffacementService {
 	@Inject
 	private NotificationService notificationService;
 	
+	@Inject
+	private IDomotiqueService domotiqueService;
+	
 	private List<User> users;
 	
 	public void effacer(long watt, int minutes) {
+		
+		
 		this.users = userRepository.findAll();
+		
+		
+		//Envoi d'une notification a tout les utilisateurs pour qui les devices vont 
+		// s'eteindre
 		for(User user : users ){
-					notificationService.sendWarning(user);
+			notificationService.sendWarning(user);
 		}
 		
 		
+		//Attente de 20Seconde
 		try {
 			Thread.sleep(20000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		for(User u : users){
+			for(Device d : u.getDevices()){
+				domotiqueService.stopDevice(d);
+			}
+		}
 	}
-
 
 	public void cancelOrder(User user) {
 		users.remove(user);
-		
+
 		for(Device d : user.getDevices()){
-			
+			domotiqueService.stopDevice(d);
 		}
 	}
 
