d35c1b16fd594ba14a8b875addefcac9d42b1b58
Romain BIARD <romain.biard@orange.com>
2013-11-23 15:33:01 +0100

    Fix Build

--- broker-api/src/main/java/com/directenergie/api/configuration/ApiConfiguration.java
+++ broker-api/src/main/java/com/directenergie/api/configuration/ApiConfiguration.java
@@ -2,11 +2,17 @@ package com.directenergie.api.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.directenergie.core.configuration.CoreConfiguration;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.directenergie.api.controller" })
+@Import(CoreConfiguration.class)
+@PropertySource("broker-api.properties")
 public class ApiConfiguration {
 
 }
